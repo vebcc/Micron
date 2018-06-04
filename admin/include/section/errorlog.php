a1d7373977cb09e2e4cbeccc70fca4466774dee9
vebcc <vebcc08@gmail.com>
2018-06-04 02:38:35 +0200

    only view error log

--- admin/include/section/errorlog.php
+++ admin/include/section/errorlog.php
@@ -7,10 +7,34 @@ if(isset($_SESSION['token']) && isset($_SESSION['login']) && isset($_SESSION['to
 ?>
 
 <div id="errorlog">
-
+    <h1>Logi</h1>
     <?php
+        if(checkpermission("section", "groupeditor")){
+            if(checkpermission("groupeditor")){
+                ?>
+                <table class='table table-striped'>
+                    <tr><th>ID</th><th>Error</th><th>IP</th><th>Data</th><th>UserAgent</th></tr>
+                <?php
+                $db_query = mysqli_query($con, "SELECT id, error, ip, date, browser FROM fail_login_ban ORDER BY date DESC");
+                while($db_row = mysqli_fetch_assoc($db_query)){
+                    $alert = "";
+                    switch($db_row['error']){
+                        case "faillogin":
+                            $alert="info";
+                            break;
+                        case "loginban":
+                            $alert="warning";
+                            break;
+                        case "interference":
+                            $alert="danger";
+                            break;
+                    }
+                    echo "<tr class='$alert'><td>".$db_row['id']."</td><td>".$db_row['error']."</td><td>".$db_row['ip']."</td><td>".$db_row['date']."</td><td>".$db_row['browser']."</td></tr>";
 
-
+                }
+                echo "</table>";
+            }
+        }
     ?>
 
 </div>
@@ -28,4 +52,3 @@ if(isset($_SESSION['token']) && isset($_SESSION['login']) && isset($_SESSION['to
     header('Location: ./');
 }
 ?>
-Error log
